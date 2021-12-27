3edd9e2f8e3d0d2637edcc51cb3d827e65f5e6fc
bassy <baberna2001@gmail.com>
2021-12-27 16:28:21 +0100

    debut ecriture lecture de fichier avec scanner

---
+++ src/Lecture.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lecture{
+
+    private String nomFichier;
+
+    private Scanner scan;
+
+    Lecture(String fichier) throws IOException {
+        this.nomFichier = fichier;
+    }
+
+    
+
+}
